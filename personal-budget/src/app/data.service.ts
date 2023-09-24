c8d369148801d77090c7bde9209b39daf3979688
Rashmitha107 <rnagirim@uncc.edu>
2023-09-23 21:04:11 -0400

    Changes based on Coding Exercise 2

---
+++ personal-budget/src/app/data.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private data= [];
+  constructor( private http: HttpClient) { }
+
+  getData() :void {
+    if(this.data.length == 0){
+      this.http.get('http://localhost:3000/budget')
+      .subscribe((res: any) => {
+        this.data = res.myBudget;
+        console.log(this.data);
+      });
+    }
+}
+  getDataSync(): any {
+    return this.data;
+  }
+
+}
